68784c9a48d74c2ff8e56e0c082b3da090feb139
KevinSipioni <kevinsipioni13@gmail.com>
2024-08-21 01:16:23 -0300

    commit

---
+++ 2 - Declaração_e_atribuição/main.go
@@ -0,0 +1,13 @@
+package main
+
+const a = "Hello, World!"
+// Declaração de uma variável 
+var b bool = true
+var c int = 10
+var d string = "Kevin"
+var e float64 = 1.2
+
+func main() {
+	a := "x"
+	println(a,b,c,d,e)
+}
