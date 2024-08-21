cd3b9126721addf0661769411041588fd53b8cef
KevinSipioni <kevinsipioni13@gmail.com>
2024-08-21 01:22:49 -0300

    commit

---
+++ 3 - Criação_de_tipos/main.go
@@ -0,0 +1,16 @@
+package main
+
+const a = "Hello, World!"
+// Criando um tipo
+type ID int
+
+var b bool = true
+var c int = 10
+var d string = "Kevin"
+var e float64 = 1.2
+var f ID = 1
+
+func main() {
+	a := "x"
+	println(a,b,c,d,e,f)
+}
