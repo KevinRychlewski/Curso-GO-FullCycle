f3767fdb3c6e7749b39dcb8dbbf8a99cce383f59
KevinSipioni <kevinsipioni13@gmail.com>
2024-08-22 00:02:00 -0300

    commit

---
+++ 4 - Importando_fmt_e_tipagem/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+const a = "Hello, World!"
+
+
+type ID int
+
+var(
+b bool = true
+c int = 10
+d string = "Kevin"
+e float64 = 1.2
+f ID = 1
+)
+
+func main() {
+
+	// %T mostra o tipo da variável
+	fmt.Printf("o tipo de E é %T", e)
+	// %v mostra o valor da variável
+	fmt.Println("o tipo de E é %v", e)
+	
+}
