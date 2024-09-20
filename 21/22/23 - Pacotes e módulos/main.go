4a1445671cdb5dfcf15aa4a78e37aff9f1a16947
KevinSipioni <kevinsipioni13@gmail.com>
2024-09-20 00:24:57 -0300

    commit

--- 21/22/23 - Pacotes e módulos/main.go
+++ 21/22/23 - Pacotes e módulos/main.go
@@ -7,6 +7,9 @@ import (
 
 func main() {
 	s := matematica.Soma(10,20)
-
+	carro := matematica.Carro{Marca: "Ford"}
 	fmt.Printf("Resultado: %v", s)
+	fmt.Println(matematica.A)
+	fmt.Println(carro)
+	fmt.Println(carro.Andar())
 }
