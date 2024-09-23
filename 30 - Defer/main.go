0282a418e62e203a77e8fe74b1ca0dc6d57b59c1
KevinSipioni <kevinsipioni13@gmail.com>
2024-09-23 00:52:29 -0300

    commmit

---
+++ 30 - Defer/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	defer fmt.Println("EU ERA A PRIMEIRA, PORA")
+	fmt.Println("Primeira linha")
+	fmt.Println("Segunda linha")
+	fmt.Println("Terceira linha")
+}
